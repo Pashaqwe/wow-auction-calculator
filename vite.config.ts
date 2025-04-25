627bfe5e085a16b91fd136bcb721f85cded61a3e
Pasha <pavel.votchinnikov@rtkit.corp>
2025-04-25 22:22:01 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/wow-auction-calculator/",
+  base: "/wow-auction-calculator",
   plugins: [react()],
 });
